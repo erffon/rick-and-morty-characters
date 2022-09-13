2179af435ae883f1bc657a398b8faaa1d1ff4b32
office <nexus1377@gmail.com>
2022-09-13 16:25:58 +0430

    character component has been made

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,10 +1,20 @@
+import Filter from "../../public/Components/UI/Filter";
 import Header from "../../public/Components/UI/Header";
+import Pagination from "../../public/Components/UI/pagination";
+import Searchbar from "../../public/Components/UI/Searchbar";
+
+const DUMMY_CHARACTERS = ['rick','morty','jerry','summer'];
 
 const characters = ()=>{
     return(
         <>
             <Header/>
-            
+            <Filter/>
+            <Searchbar/>
+            {DUMMY_CHARACTERS.map((item,index)=>{
+                return <li key={index}>{item}</li>;
+            })}
+            <Pagination/>
         </>
     );
 }
---
+++ public/Components/UI/Filter.tsx
@@ -0,0 +1,12 @@
+const Filter = ()=>{
+    return(
+        <>
+            <img src="https://img.icons8.com/ios/344/sorting-options--v1.png"/>
+            <div>
+                <button>name</button>
+                <button>gender</button>
+            </div>
+        </>
+    );
+}
+export default Filter;
